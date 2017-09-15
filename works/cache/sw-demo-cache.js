cd43079881bbb6aef149626bad3cd7320d9a4407
zyh <617754841@qq.com>
2017-09-15 17:04:11 +0800

    service worker

---
+++ works/cache/sw-demo-cache.js
@@ -0,0 +1,47 @@
+var VERSION = 'v1';
+
+// 缓存
+self.addEventListener('install', function(event) {
+	console.log('install', event)
+  event.waitUntil(
+    caches.open(VERSION).then(function(cache) {
+      return cache.addAll([
+        './index.html',
+        './static/jquery.min.js',
+        './static/mm1.jpg'
+      ]);
+    })
+  );
+});
+
+// 缓存更新
+self.addEventListener('activate', function(event) {
+	console.log('activate', event)
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          // 如果当前版本和缓存版本不一致
+          if (cacheName !== VERSION) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 捕获请求并返回缓存数据
+self.addEventListener('fetch', function(event) {
+	console.log('fetch', event)
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(response) {
+    caches.open(VERSION).then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }).catch(function() {
+    return caches.match('./static/mm1.jpg');
+  }));
+});
