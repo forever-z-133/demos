0d3cd87f009bafd82aefbb5833089578e15e0bf6
zyh <617754841@qq.com>
2017-09-15 18:10:27 +0800

    不知道什么错

--- works/cache/sw-demo-cache.js
+++ works/cache/sw-demo-cache.js
@@ -6,7 +6,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(VERSION).then(function(cache) {
       return cache.addAll([
-        './index.html',
+        './start.html',
         './static/jquery.min.js',
         './static/mm1.jpg'
       ]);
