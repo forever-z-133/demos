462ce275dcaba98804ddcc8a78cb795687407145
yonghzhang <yonghzhang@tencent.com>
2025-02-08 18:09:18 +0800

    feat: 新增递归组件，未完成

--- src/src/components/index.ts
+++ src/src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as ColumnItem } from './column/column-item.vue'
 export { default as Column } from './column/column.vue'
 export { default as CommonList } from './common-list/index.vue'
+export { default as RecursionFlatLoop } from './recursion-loop/flat.vue'
+export { default as RecursionLoop } from './recursion-loop/index.vue'
---
+++ src/src/components/recursion-loop/flat.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  data?: any[]
+}
+defineOptions({
+  name: 'RecursionFlatLoop',
+})
+const props = withDefaults(defineProps<Props>(), {
+  data: () => [],
+})
+
+const propArr = computed(() => {
+  return props.data.slice(1, props.data.length)
+})
+</script>
+
+<template>
+  <div class="box">
+    <RecursionFlatLoop v-if="propArr.length !== 0" :data="propArr" />
+    {{ props.data }}
+  </div>
+</template>
+
+<style>
+.box {
+  width: 100%;
+  height: 100%;
+  padding: 5px;
+  border: 1px solid red;
+}
+</style>
