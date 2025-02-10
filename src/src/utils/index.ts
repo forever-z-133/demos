1836c7661cf0353cd3cddb410401a76f5ef376fe
yonghzhang <yonghzhang@tencent.com>
2025-02-10 16:18:32 +0800

    feat: 新增无限滚动效果，新增部分公共函数

--- src/src/utils/index.ts
+++ src/src/utils/index.ts
@@ -1,6 +1,21 @@
 // 获取随机数
-export function random(min: number, max: number) {
-  return Math.random() * (max - min) + min
+export function random(n1: number, n2 = 0) {
+  const min = Math.min(n1, n2)
+  const max = Math.max(n1, n2)
+  return min + Math.random() * (max - min)
+}
+
+// 获取随机颜色
+export function randomColor() {
+  // return `#${Math.random().toString(16).slice(2, 8)}`
+  return `hsla(${Math.floor(Math.random() * 360)}, 50%, 90%, 1)`
+}
+
+// 补零
+export function addZero(num: number | string, len = 2) {
+  let numLen = `${num}`.length
+  while (numLen++ < len) num = `0${num}`
+  return `${num}`
 }
 
 // 获取鼠标或手势位置
---
+++ src/src/views/pure-css-infinite-marquee/index.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { addZero } from '@/utils'
+import InfiniteMarquee from './InfiniteMarquee.vue'
+
+defineOptions({
+  title: '纯 CSS 实现无限滚动',
+  group: 'effect',
+})
+
+const colors = ['pink', 'lightgray', 'lightblue', 'lightgreen', 'lightcyan']
+const numbers = Array.from({ length: 10 }, (_, i) => `100000000${addZero(i + 1)}`)
+
+const code = `<template>
+  <div class="infinite-marquee">
+    <div class="wrapper">
+      <slot />
+    </div>
+    <div class="wrapper" aria-hidden>
+      <slot />
+    </div>
+  </div>
+<template>
+
+<style lang="less">
+.infinite-marquee {
+  display: flex;
+  --animation-duration: 10s;
+
+  & > .wrapper {
+    flex-shrink: 0;
+    display: flex;
+    animation: scrolling var(--animation-duration) linear infinite;
+  }
+}
+
+@keyframes scrolling {
+  to { transform: translateX(-100%); }
+}
+</style>
+`
+</script>
+
+<template>
+  <div class="pure-css-infinite-marquee">
+    <p class="block">
+      纯 CSS 实现无限滚动。靠拷贝一份子元素，完成前后尾的拼接，然后进行 <code>transform: translateX(-100%)</code> 的动画即可。
+    </p>
+    <p class="block">
+      注意：动画时长应当随子元素宽度而变化，需动态计算。
+    </p>
+    <div class="case">
+      <InfiniteMarquee>
+        <template v-for="(color, index) in colors" :key="color">
+          <div class="card" :style="{ background: color }">
+            {{ index }}
+          </div>
+        </template>
+      </InfiniteMarquee>
+    </div>
+    <div class="case">
+      <InfiniteMarquee>
+        <template v-for="num in numbers" :key="num">
+          <span class="number">{{ num }}</span>
+        </template>
+      </InfiniteMarquee>
+    </div>
+    <div class="block">
+      <pre><code>{{ code }}</code></pre>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+@import "@/styles/mixins.less";
+
+.pure-css-infinite-marquee {
+  .items-gap(bottom);
+
+  .card {
+    width: .px(100)[];
+    height: .px(100)[];
+    margin-right: .px(10)[];
+  }
+
+  .number {
+    margin-right: .px(10)[];
+  }
+}
+</style>
